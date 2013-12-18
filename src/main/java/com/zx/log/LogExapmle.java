d60046dd89f30e9d3265bec1b67a52bf8563e65c
zhangx <zhangxinxf@163.com>
2013-12-18 19:31:40 +0800

    ad

--- src/main/java/com/zx/log/LogExapmle.java
+++ src/main/java/com/zx/log/LogExapmle.java
@@ -3,11 +3,20 @@ package com.zx.log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 日志
+ * 
+ * @author 2013
+ * 
+ */
 public class LogExapmle {
    private static Logger logger = LoggerFactory.getLogger(LogExapmle.class);
 
    public static void main(String[] args) {
-      Object o=12;
-      logger.warn("this check {} ",o);
+      Object o = 12;
+      logger.debug("this debug {} ", o);
+      logger.info("this info {} ", o);
+      logger.warn("this warn {} ", o);
+      logger.error("this error {} ", o);
    }
 }
